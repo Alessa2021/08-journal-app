db9549ae6fccb04998156712d4f5107dd2c21a57
Alejandra Casado <z.prende.a@gmail.com>
2022-08-16 14:50:05 -0300

    archivos del curso hasta video 272

---
+++ src/firebase/providers.js
@@ -0,0 +1,44 @@
+import {GoogleAuthProvider, signInWithPopup, updateProfile} from 'firebase/auth'
+import { FirebaseAuth } from './config'
+
+const googleProvider = new GoogleAuthProvider()
+
+export const signInWithGoogle = async () => {
+
+    try {
+        
+        const result = await signInWithPopup(FirebaseAuth, googleProvider)
+        //const credentials = GoogleAuthProvider.credentialFromResult( result )
+        
+        const { displayName, email, photoURL, uid} = result.user
+
+        console.log({user})
+
+        return {
+            ok: true,
+            displayName, email, photoURL, uid
+        }
+
+
+    } catch (error) {
+
+        const errorCode = error.errorCode
+        const errorMessage = error.message
+        const email = error.customData.email
+        const credential = GoogleAuthProvider.credentialFromError(error)
+
+
+        console.log(error)
+
+        return {
+            ok: false,
+            errorMessage,
+            errorCode,
+            email,
+            credential
+
+        }
+        
+    }
+
+}
